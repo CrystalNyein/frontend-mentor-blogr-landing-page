394003f37d9cbfa1dcc3911b7a392fa67fe88266
CrystalNyein <nyeinchanlay99@gmail.com>
2021-12-12 23:30:25 +0630

    Toggle collapse menu done

--- src/components/CollapseNav.js
+++ src/components/CollapseNav.js
@@ -1,22 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ArrowDown from "../assets/images/icon-arrow-light.svg";
 import "./CollapseNav.css";
 
 const CollapseNav = (props) => {
   const [collapsed, setCollapsed] = useState(true);
-  const openModal = (e) => {
-    console.log(e.target);
+  const titleRef = useRef(null);
+  const collapseRef = useRef(null);
+  const toggleModal = (e) => {
     setCollapsed(!collapsed);
-    e.target.classList.add("collapse");
+    if (titleRef.current.classList.contains("collapse")) {
+      titleRef.current.classList.remove("collapse");
+    } else {
+      titleRef.current.classList.add("collapse");
+    }
   };
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (!titleRef.current.contains(e.target))
+        if (collapseRef.current && !collapseRef.current.contains(e.target)) {
+          toggleModal();
+        }
+    };
+    if (!collapsed) document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  });
   return (
     <div className="CollapseNav">
-      <p className="nav-title f-overpass" onClick={openModal}>
+      <p className="nav-title f-overpass" ref={titleRef} onClick={toggleModal}>
         {props.name}{" "}
       </p>
       <img className="collapsed" src={ArrowDown} alt=""></img>
       {!collapsed && (
-        <div className="collapse-container">
+        <div className="collapse-container" ref={collapseRef}>
           {props.collapse.map((c, i) => (
             <a key={i} href="#" className="f-overpass">
               {c}{" "}
