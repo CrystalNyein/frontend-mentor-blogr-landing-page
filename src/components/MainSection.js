23938304125140b6207e590f0c7ee9736a271a23
CrystalNyein <nyeinchanlay99@gmail.com>
2021-12-14 23:47:57 +0630

    Mobile design done

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import FeatureText from "./FeatureText";
 import "./MainSection.css";
-import EditorImg from "../assets/images/illustration-editor-desktop.svg";
+import EditorImgDsk from "../assets/images/illustration-editor-desktop.svg";
+import EditorImgMb from "../assets/images/illustration-editor-mobile.svg";
 import PhoneImg from "../assets/images/illustration-phones.svg";
-import LaptopImg from "../assets/images/illustration-laptop-desktop.svg";
+import LaptopImgDsk from "../assets/images/illustration-laptop-desktop.svg";
+import LaptopImgMb from "../assets/images/illustration-laptop-mobile.svg";
 import { features } from "../constants/Features";
 
 const MainSection = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   return (
     <main className="MainSection">
       <section className="section1">
         <h2 className="f-overpass">Designed for the future</h2>
         <div className="section-body">
-          <img className="section-img" src={EditorImg} alt="Editor"></img>
+          <img
+            className="section-img"
+            src={windowWidth < 650 ? EditorImgMb : EditorImgDsk}
+            alt="Editor"
+          ></img>
           <div className="section-text">
             <FeatureText title={features[0].title} text={features[0].text} />
             <FeatureText title={features[1].title} text={features[1].text} />
@@ -28,7 +35,11 @@ const MainSection = () => {
 
       <section className="section3">
         <div className="section-body">
-          <img className="section-img" src={LaptopImg} alt="Laptop"></img>
+          <img
+            className="section-img"
+            src={windowWidth < 650 ? LaptopImgMb : LaptopImgDsk}
+            alt="Laptop"
+          ></img>
           <div className="section-text">
             <FeatureText title={features[3].title} text={features[3].text} />
             <FeatureText title={features[4].title} text={features[4].text} />
