84bf633745b2bd78f712fd9e14d535ef0476d4e0
CrystalNyein <nyeinchanlay99@gmail.com>
2021-12-14 21:31:59 +0630

    Linked footer section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import FooterSection from "./components/FooterSection";
 import HeroSection from "./components/HeroSection";
 import MainSection from "./components/MainSection";
 
@@ -7,6 +8,7 @@ const App = () => {
     <div className="App">
       <HeroSection />
       <MainSection />
+      <FooterSection />
     </div>
   );
 };
